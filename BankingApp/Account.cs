d3f8677d7830f8e925a337a73ff943cffaa5f5b7
matt-r-mck <matt.r.mck@gmail.com>
2020-06-18 19:19:56 -0400

    Add project files.

---
+++ BankingApp/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingApp {
+    class Account {
+
+        public int AccountNumber { get; set; }
+        public double Balance { get; set; }
+        public string Description { get; set; }
+
+        public void Deposit(double Ammount) {
+            if (Ammount < 0) { 
+                Console.WriteLine("ERROR: Ammount must be positive");
+            return;
+        }
+            Balance += Ammount;
+        }
+
+        public void Withdraw(double Ammount) {
+            if (Ammount <= 0) {
+                Console.WriteLine("Error: can only enter positive number");
+                return; }
+            else 
+            if (Ammount > Balance) {
+                Console.WriteLine("ERROR: Insufficient Funds");
+                return;
+            }
+                Balance -= Ammount;
+        }
+
+        public double CheckBalance() {
+            return Balance;
+        }
+
+        public Account() { //default constructor sets initial name for account
+            this.AccountNumber = 0;
+            this.Description = "New Account";
+        }
+
+    }
+}
---
+++ BankingApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankingApp {
+    class Program {
+        static void Main(string[] args) {
+
+            var acct1 = new Account();
+
+            acct1.Deposit(100000);
+            acct1.Withdraw(60000);
+            acct1.Withdraw(50000);
+            acct1.Deposit(-20000);
+            acct1.Withdraw(-50000);
+        }
+    }
+}
